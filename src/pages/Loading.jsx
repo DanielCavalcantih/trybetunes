557902ea958490109cbea31ad0ea63088cfe0886
Daniel Cavalcanti <danielcavalcanti8000@gmail.com>
2022-05-18 17:48:15 -0300

    Requisito 2

---
+++ src/pages/Loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Loading extends React.Component {
+  render() {
+    return (
+      <span>Carregando...</span>
+    );
+  }
+}
+
+export default Loading;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,74 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+import { createUser } from '../services/userAPI';
+import Loading from './Loading';
 
 class Login extends React.Component {
+  state = {
+    nome: '',
+    loading: false,
+    buttonDisabled: true,
+    userCreated: false,
+  }
+
+  handleChange = ({ target }) => {
+    const { value } = target;
+    const maxLength = 3;
+
+    this.setState({
+      nome: value,
+    });
+
+    if (value.length >= maxLength) {
+      this.setState({
+        buttonDisabled: false,
+      });
+    }
+  }
+
+  getCreatUser = async () => {
+    const { nome } = this.state;
+    this.setState({
+      loading: true,
+    });
+    await createUser({ name: nome });
+    this.setState({
+      userCreated: true,
+    });
+  }
+
   render() {
-    return (
-      <div data-testid="page-login">
-        <h2>Login</h2>
-      </div>
-    );
+    const { buttonDisabled, nome, loading, userCreated } = this.state;
+
+    if (!loading) {
+      return (
+        <div data-testid="page-login">
+          <h2>Login</h2>
+          <form>
+            <input
+              type="text"
+              data-testid="login-name-input"
+              value={ nome }
+              onChange={ this.handleChange }
+            />
+            <button
+              type="button"
+              data-testid="login-submit-button"
+              disabled={ buttonDisabled }
+              onClick={ this.getCreatUser }
+            >
+              Entrar
+            </button>
+          </form>
+        </div>
+      );
+    }
+
+    if (loading && !userCreated) {
+      return <Loading />;
+    }
+
+    return <Redirect to="/search" />;
   }
 }
 
