8ed9aa2ac0bc6285ac25c65fa7c1450897ca250c
Daniel Cavalcanti <danielcavalcanti8000@gmail.com>
2022-05-18 22:13:33 -0300

    Requisito 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,10 +2,38 @@ import React from 'react';
 import Header from '../Components/Header';
 
 class Search extends React.Component {
+  state = {
+    searchInput: '',
+    buttonDisabled: true,
+  }
+
+  handleChange = ({ target }) => {
+    const { value } = target;
+    this.setState({
+      searchInput: value,
+      buttonDisabled: value.length <= 1,
+    });
+  }
+
   render() {
+    const { buttonDisabled, searchInput } = this.state;
+
     return (
       <div data-testid="page-search">
         <Header />
+        <input
+          type="text"
+          data-testid="search-artist-input"
+          value={ searchInput }
+          onChange={ this.handleChange }
+        />
+        <button
+          type="button"
+          data-testid="search-artist-button"
+          disabled={ buttonDisabled }
+        >
+          Pesquisar
+        </button>
       </div>
     );
   }
