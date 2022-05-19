5f9c7e6e5ba6a286c3a5c09339f970ede213479e
Daniel Cavalcanti <danielcavalcanti8000@gmail.com>
2022-05-19 18:35:30 -0300

    Requisito 6

--- src/App.js
+++ src/App.js
@@ -10,19 +10,16 @@ import Search from './pages/Search';
 class App extends React.Component {
   render() {
     return (
-      <>
-        <p>TrybeTunes</p>
-        <BrowserRouter>
-          <Switch>
-            <Route exact path="/" component={ Login } />
-            <Route path="/search" component={ Search } />
-            <Route path="/album/:id" component={ Album } />
-            <Route path="/favorites" component={ Favorites } />
-            <Route path="/profile" component={ Profile } />
-            <Route path="*" component={ NotFound } />
-          </Switch>
-        </BrowserRouter>
-      </>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          <Route path="/search" component={ Search } />
+          <Route path="/album/:id" component={ Album } />
+          <Route path="/favorites" component={ Favorites } />
+          <Route path="/profile" component={ Profile } />
+          <Route path="*" component={ NotFound } />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,40 +1,105 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../Components/Header';
+import Loading from '../Components/Loading';
+import searchAlbumsAPIs from '../services/searchAlbumsAPI';
 
 class Search extends React.Component {
   state = {
     searchInput: '',
+    searchSaved: '',
     buttonDisabled: true,
+    loading: false,
+    buttonActive: false,
+    albums: [],
   }
 
   handleChange = ({ target }) => {
     const { value } = target;
     this.setState({
       searchInput: value,
+      searchSaved: value,
       buttonDisabled: value.length <= 1,
     });
   }
 
+  searchArtist = async () => {
+    const { searchInput } = this.state;
+
+    this.setState({
+      buttonActive: true,
+    });
+
+    this.setState({
+      searchInput: '',
+      searchSaved: searchInput,
+      loading: true,
+    });
+
+    const response = await searchAlbumsAPIs(searchInput);
+
+    this.setState({
+      albums: response,
+      loading: false,
+    });
+  }
+
   render() {
-    const { buttonDisabled, searchInput } = this.state;
+    const {
+      buttonDisabled,
+      searchInput,
+      loading,
+      searchSaved,
+      buttonActive,
+      albums } = this.state;
+
+    if (!loading) {
+      return (
+        <div data-testid="page-search">
+          <Header />
+          <input
+            type="text"
+            data-testid="search-artist-input"
+            value={ searchInput }
+            onChange={ this.handleChange }
+          />
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ buttonDisabled }
+            onClick={ this.searchArtist }
+          >
+            Pesquisar
+          </button>
+          {buttonActive && (
+            <h3>
+              Resultado de álbuns de:
+              { ` ${searchSaved}` }
+            </h3>
+          )}
+          {!albums.length ? (
+            <span>Nenhum álbum foi encontrado</span>
+          ) : (
+            <ul>
+              {albums.map((album) => (
+                <Link
+                  key={ album.collectionId }
+                  to={ `/album/${album.collectionId}` }
+                  data-testid={ `link-to-album-${album.collectionId}` }
+                >
+                  <li>
+                    <p>{ album.collectionName }</p>
+                    <img src={ album.artworkUrl100 } alt="" />
+                  </li>
+                </Link>
+              ))}
+            </ul>)}
+        </div>
+      );
+    }
 
     return (
-      <div data-testid="page-search">
-        <Header />
-        <input
-          type="text"
-          data-testid="search-artist-input"
-          value={ searchInput }
-          onChange={ this.handleChange }
-        />
-        <button
-          type="button"
-          data-testid="search-artist-button"
-          disabled={ buttonDisabled }
-        >
-          Pesquisar
-        </button>
-      </div>
+      <Loading />
     );
   }
 }
